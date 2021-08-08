61d9f62a4020d37d6b9d8ce25b1f1ca158866c1d
Greidy Peña <greidykp@gmail.com>
2021-08-07 20:14:26 -0500

    Event deposit and withdraw for non-existing account

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -2,9 +2,43 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Account;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\AcceptHeader;
 
 class EventController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        if ($request->type === 'deposit'){
+            return $this->deposit (
+                $request->destination,
+                $request->amount
+            );
+        } elseif ($request->type === 'withdraw') {
+            return $this->withdraw (
+                $request->origin,
+                $request->amount
+            );
+        }
+    }
+
+    private function deposit($destination, $amount)
+    {
+        $account = Account::firstOrCreate([ 'id' => $destination], []);
+        $account->balance += $amount;
+        $account->save();
+
+        return response()->json([
+            'destination' => [
+                'id' => $account->id,
+                'balance' => $account->balance
+            ]
+            ], 201);
+    }
+
+    private function withdraw($origin, $amount)
+    {
+        $account = Account::findOrFail($origin);
+    }
 }
